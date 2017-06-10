d72515ab8667c1cc6d9e65fa7972f6258143993b
zijie0 <myoilbox@gmail.com>
2017-06-10 11:44:28 +0800

    test

---
+++ src/main/java/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by Bytes on 2017/6/10.
+ */
+public class test {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String a = "hello world";
+        System.out.print(a);
+    }
+}
